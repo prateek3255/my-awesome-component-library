4c5d53e1d74ff49eb9c2763413d33cfe9ae54a58
Prateek Surana <prateeksurana3255@gmail.com>
2020-12-07 17:44:06 +0530

    :sparkles: Added button stories

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Meta } from "@storybook/react/types-6-0";
+import { Story } from "@storybook/react";
+import { Button, ButtonProps } from "./Button";
+
+export default {
+  title: "Components/Button",
+  component: Button,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as Meta;
+
+// Create a master template for mapping args to render the Button component
+const Template: Story<ButtonProps> = (args) => <Button {...args} />;
+
+// Reuse that template for creating different stories
+export const Primary = Template.bind({});
+Primary.args = { label: "Primary 😃", size: "large" };
+
+export const Secondary = Template.bind({});
+Secondary.args = { ...Primary.args, primary: false, label: "Secondary 😇" };
