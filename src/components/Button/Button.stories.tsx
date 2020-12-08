af7ef6715e006db4c5a2f6fecb6212a0fc928d0a
Prateek Surana <prateeksurana3255@gmail.com>
2020-12-08 17:39:57 +0530

    :recycle: Fix Button import in stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Meta } from "@storybook/react/types-6-0";
 import { Story } from "@storybook/react";
-import { Button, ButtonProps } from "./Button";
+import Button, { ButtonProps } from "./Button";
 
 export default {
   title: "Components/Button",
