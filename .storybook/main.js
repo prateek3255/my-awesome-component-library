5f7703fa13a9035a6f4345d7daf19b3a403e4070
Prateek Surana <prateeksurana3255@gmail.com>
2023-02-03 00:29:47 +0530

    ⬆️ Upgrade all dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,10 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.tsx"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
-}
+  core: {
+    builder: 'webpack5'
+  },
+  "stories": ["../src/**/*.stories.tsx"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-postcss"],
+  docs: {
+    autodocs: true
+  }
+};
