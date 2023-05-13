ef9447ebcac6bb864141434f343ac4f4e6c7e80c
Prateek Surana <prateeksurana3255@gmail.com>
2023-05-14 00:20:43 +0530

    💡 Add relevant comments

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,6 +7,7 @@ import dts from "rollup-plugin-dts";
 
 // This is required to read package.json file when
 // using Native ES modules in Node.js
+// https://rollupjs.org/command-line-interface/#importing-package-json
 import { createRequire } from 'node:module';
 const requireFile = createRequire(import.meta.url);
 const packageJson = requireFile('./package.json');
--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import type { Meta, StoryObj } from "@storybook/react";
 import Button from "./Button";
 
+// Default metadata of the story https://storybook.js.org/docs/react/api/csf#default-export
 const meta: Meta<typeof Button> = {
   title: "Components/Button",
   component: Button,
@@ -13,9 +14,9 @@ const meta: Meta<typeof Button> = {
 
 export default meta;
 
+// The story type for the component https://storybook.js.org/docs/react/api/csf#named-story-exports
 type Story = StoryObj<typeof Button>;
 
-// Reuse that template for creating different stories
 export const Primary: Story = {
   args: {
     label: "Primary 😃",
